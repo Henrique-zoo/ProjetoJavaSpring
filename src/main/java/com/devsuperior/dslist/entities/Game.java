7daa3bc26c4a4e469fa4044d6ae29c1a8eb6a671
Henrique-zoo <morcelleshenrique@gmail.com>
2023-07-13 21:15:20 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -21,16 +21,17 @@ public class Game {
 	private Integer year;
 	private String genre;
 	private String platforms;
-	private double score;
+	private Double score;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	// Cria o método construtor sem receber nada
 	public Game() {
 	}
 	// Cria o método construtor recebendo argumentos
-	public Game(Long id, String title, Integer year, String genre, String platforms, double score, String imgUrl,
-			String shortDescription, String longDescription) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
@@ -75,7 +76,7 @@ public class Game {
 	public double getScore() {
 		return score;
 	}
-	public void setScore(double score) {
+	public void setScore(Double score) {
 		this.score = score;
 	}
 	public String getImgUrl() {
@@ -112,6 +113,4 @@ public class Game {
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
 }
